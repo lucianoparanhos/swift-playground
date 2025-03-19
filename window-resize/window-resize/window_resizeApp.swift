00ed60ff7b8fe348b3e9f17f6c0c0f581eaa75c6
Luciano Paranhos <luciano@paranhos.com.br>
2025-03-18 21:34:33 -0300

    Exemplo de como redimencionar a janela do aplicativo com efeito suave

---
+++ window-resize/window-resize/window_resizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  window_resizeApp.swift
+//  window-resize
+//
+//  Created by Luciano Paranhos on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct window_resizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
